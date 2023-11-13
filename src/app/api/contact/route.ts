7a5d14db32d0f9f00c222b134fabc6219999540e
AdrianKnapp <adrknapp@gmail.com>
2023-11-13 09:29:07 -0300

    feat: added awaits

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -4,30 +4,33 @@ import { Resend } from 'resend';
 
 const resendApiKey = process.env.RESEND_API_KEY
 
-const sendFeedbackEmail = ({
+const sendFeedbackEmail = async ({
   name,
   email,
 }: Contact, resend: Resend) => {
-  resend.emails.send({
+  const data = await resend.emails.send({
     from: 'contato@controlgameapp.com',
     to: email,
     subject: 'Contato',
     html: `<p>Olá ${name}, agradecemos pelo contato! <br />Sua mensagem foi recebida e será respondida em breve.</p>`,
   });
+
+  console.log("🚀 ~ file: route.ts:17 ~ data:", data)
 }
 
-const sendEmailToUs = ({
+const sendEmailToUs = async ({
   name,
   email,
   content
 }: Contact, resend: Resend) => {
-  
-  resend.emails.send({
+  const data = await resend.emails.send({
     from: 'contato@controlgameapp.com',
     to: ['adrknapp@gmail.com', 'thugtroll8@gmail.com'],
     subject: 'Contato',
     html: `<p>E-mail: ${email} <br /> Nome: ${name} <br /> Conteúdo: ${content} </p>`
   });
+  
+  console.log("🚀 ~ file: route.ts:31 ~ data:", data)
 }
 
 export async function POST(request: Request) {
@@ -37,8 +40,8 @@ export async function POST(request: Request) {
 
   const resend = new Resend(resendApiKey);
 
-  sendFeedbackEmail(contact, resend)
-  sendEmailToUs(contact, resend)
+  await sendFeedbackEmail(contact, resend)
+  await sendEmailToUs(contact, resend)
 
   return NextResponse.json(
     { message: `E-mail successfully sent to ${contact.email}.` },
