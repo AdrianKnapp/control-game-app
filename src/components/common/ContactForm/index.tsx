403e843d4689d5e88d10102a3b9003f910a3b2c6
AdrianKnapp <adrknapp@gmail.com>
2023-11-12 22:18:06 -0300

    feat: added feedback initial

--- src/components/common/ContactForm/index.tsx
+++ src/components/common/ContactForm/index.tsx
@@ -6,6 +6,7 @@ import { Textarea } from '@/components/ui/Textarea'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+import { useState } from 'react'
 
 type Inputs = {
   name: string
@@ -27,6 +28,7 @@ export const ContactForm = () => {
   } = useForm<Inputs>({
     resolver: yupResolver(schema)
   })
+  const [isEmailSent, setIsEmailSent] = useState(false)
 
   const onSubmit: SubmitHandler<Inputs> = async data => {
     try {
@@ -40,44 +42,51 @@ export const ContactForm = () => {
       })
 
       console.log('success')
+      setIsEmailSent(true)
     } catch (err) {
       console.log(err)
     }
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col gap-3 rounded-xl bg-white p-5 lg:gap-5"
-    >
-      <p className="text-center text-lg font-semibold text-teal-700">
-        Entre em contato
-      </p>
-      <div className="flex flex-col gap-3 lg:flex-row lg:gap-5">
-        <Input
-          label="Nome"
-          placeholder="Qual é o seu nome?"
-          {...register('name')}
-          error={errors.name ? 'Campo inválido' : undefined}
-        />
-        <Input
-          label="E-mail"
-          type="email"
-          placeholder="Seu melhor e-mail"
-          {...register('email')}
-          error={errors.email ? 'Campo inválido' : undefined}
-        />
-      </div>
-      <Textarea
-        label="Assunto"
-        id="content"
-        placeholder="Como podemos te ajudar?"
-        {...register('content')}
-        error={errors.content ? 'Campo inválido' : undefined}
-      />
-      <div className="mt-2">
-        <Button type="submit">Enviar</Button>
-      </div>
-    </form>
+    <section className="rounded-xl bg-white p-5">
+      {isEmailSent ? (
+        <p>E-mail enviado com sucesso.</p>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-3 lg:gap-5"
+        >
+          <p className="text-center text-lg font-semibold text-teal-700">
+            Entre em contato
+          </p>
+          <div className="flex flex-col gap-3 lg:flex-row lg:gap-5">
+            <Input
+              label="Nome"
+              placeholder="Qual é o seu nome?"
+              {...register('name')}
+              error={errors.name ? 'Campo inválido' : undefined}
+            />
+            <Input
+              label="E-mail"
+              type="email"
+              placeholder="Seu melhor e-mail"
+              {...register('email')}
+              error={errors.email ? 'Campo inválido' : undefined}
+            />
+          </div>
+          <Textarea
+            label="Assunto"
+            id="content"
+            placeholder="Como podemos te ajudar?"
+            {...register('content')}
+            error={errors.content ? 'Campo inválido' : undefined}
+          />
+          <div className="mt-2">
+            <Button type="submit">Enviar</Button>
+          </div>
+        </form>
+      )}
+    </section>
   )
 }
